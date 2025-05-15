0091873b84207c7d647ffa25f9febe0987ab584a
FrancoMarchetta <frncmarchetta@gmail.com>
2025-05-15 15:38:23 -0300

    finished most of the design

--- src/assets/Pages/Home.tsx
+++ src/assets/Pages/Home.tsx
@@ -2,6 +2,7 @@ import Card from "../components/Card"
 import NavBar from "../components/NavBar"
 import "../Animations.css"
 import SplitText from "../components/SplitText"
+//technologies
 import js from "../images/js.png"
 import ts from "../images/ts.png"
 import css from "../images/css.png"
@@ -12,18 +13,31 @@ import figma from "../images/figma.png"
 import supabase from "../images/supabase.png"
 import reactjs from "../images/react.png"
 import sql from "../images/sql.png"
+//contact
+import email from "../images/email.svg"
+import linkedin from "../images/linkedin.svg"
+import github from "../images/github.svg"
+
 
 function Home() {
     return (
         <>
             <main className="h-dvh flex flex-col gap-8 ">
-                <div className="h-30 self-end mr-8 " style={{ top: "0", position: "sticky" }}>
-                    <NavBar></NavBar>
+                <div className="h-30 self-end mr-8 " style={{ top: "0" }}>
+                    <NavBar>
+                        <a href="#home"><p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Inicio</p></a>
+                        <a href="#projects"><p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Projectos</p></a>
+                        <a href="#technologies"><p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Tecnologias</p></a>
+                        <p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Contacto</p>
+                        <select className="hover:text-blue-400 cursor-pointer" name="" id="">
+                            <option className='text-black' value="">Español</option>
+                            <option className='text-black' value="">English</option>
+                        </select>
+
+                    </NavBar>
                 </div>
 
-                <section>
-
-
+                <section id="home">
                     <div className=" lg:w-300 flex justify-center">
                         <SplitText
                             className="text-7xl drop-shadow-lg"
@@ -56,7 +70,7 @@ function Home() {
                 </div>
 
 
-                <section className="flex flex-col lg:flex-row gap-10 justify-center items-center">
+                <section id="projects" className="flex flex-col lg:flex-row gap-10 justify-center items-center">
                     <Card></Card>
                     <Card></Card>
                     <Card></Card>
@@ -69,18 +83,18 @@ function Home() {
                     }}>Tecnologías</h1>
                 </div>
 
-                <section>
+                <section id="technologies">
 
                     <div id="technologies-container" className="text-amber-50 w-200 flex place-self-center flex-wrap flex-col lg:flex-row gap-10 justify-center items-center">
                         <div className="flex flex-col items-center">
                             <img className="w-20 h-20" src={js} alt="Javascript" />
                             <p>Javascript</p>
                         </div>
-                         <div className="flex flex-col items-center">
+                        <div className="flex flex-col items-center">
                             <img className="w-20 h-20" src={reactjs} alt="React" />
                             <p>React</p>
                         </div>
-                       
+
                         <div className="flex flex-col items-center">
                             <img className="w-20 h-20" src={css} alt="CSS" />
                             <p>CSS</p>
@@ -101,7 +115,7 @@ function Home() {
                             <img className="w-20 h-20" src={figma} alt="Figma" />
                             <p>Figma</p>
                         </div>
-                         <div className="flex flex-col items-center">
+                        <div className="flex flex-col items-center">
                             <img className="w-20 h-20" src={ts} alt="Typescript" />
                             <p>Typescript</p>
                         </div>
@@ -109,18 +123,34 @@ function Home() {
                             <img className="w-20 h-20" src={supabase} alt="Supabase" />
                             <p>Supabase</p>
                         </div>
-                       
+
                         <div className="flex flex-col items-center">
                             <img className="w-20 h-20" src={sql} alt="SQL" />
                             <p>SQL</p>
                         </div>
                     </div>
-
-
                 </section>
 
+                <div className=" min-h-0.5 bg-amber-50 flex justify-center items-center text-4xl "></div> {/** <-- separador */}
 
-            </main>
+                <section className="flex gap-33 text-amber-50 lg:flex-row gap-10 justify-center items-center">
+                    <div className="w-25 text-center group">
+                        <img src={github} alt="" className="brightness-0 invert hover:brightness-100 hover:invert-0 transition-all duration-300" />
+                        <p>GitHub</p>
+                    </div>
+
+                    <div className="w-25 text-center group">
+                        <img src={linkedin} alt="" className="brightness-0 invert hover:brightness-100 hover:invert-0 transition-all duration-300" />
+                        <p>LinkedIn</p>
+                    </div>
+
+                    <div className="w-25 text-center group">
+                        <img src={email} alt="" className="brightness-0 invert hover:brightness-100 hover:invert-0 transition-all duration-300" />
+                        <p>Email</p>
+                    </div>
+                </section>
+
+            </main >
         </>
     )
 }
--- src/assets/components/NavBar.tsx
+++ src/assets/components/NavBar.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function NavBar() {
+function NavBar({ children }: any) {
     return (
-        <main id='nav-bar' className='w-150 h-15 justify-center place-items-center text-amber-50 flex gap-10 font-bold float-end text-lg -translate-x-10 translate-y-5' >
-            <p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Inicio</p>
-            <p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Projectos</p>
-            <p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Tecnologias</p>
-            <p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Contacto</p>
-            <select name="" id="">
-                <option className='text-black' value="">Español</option>
-                <option className='text-black' value="">English</option>
-            </select>
+        <main
+            id='nav-bar'
+            className='fixed z-50 w-150 h-15 justify-center place-items-center text-amber-50 flex gap-10 font-bold text-lg bg-black/80 backdrop-blur-sm px-4 py-2 rounded-lg'
+            style={{
+                top: '20px',
+                right: '40px'
+            }}
+        >
+            {children}
         </main>
     )
 }

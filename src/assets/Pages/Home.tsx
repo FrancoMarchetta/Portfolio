2f1271493c4a2d513e39de5dbfae58b1f100ec16
FrancoMarchetta <frncmarchetta@gmail.com>
2025-05-16 13:31:29 -0300

    changed card design

--- src/assets/Pages/Home.tsx
+++ src/assets/Pages/Home.tsx
@@ -2,6 +2,11 @@ import Card from "../components/Card"
 import NavBar from "../components/NavBar"
 import "../Animations.css"
 import SplitText from "../components/SplitText"
+
+//project assets
+import blackjack from "../images/blackjack.png";
+import inmovw from "../images/inmovw.png";
+
 //technologies
 import js from "../images/js.png"
 import ts from "../images/ts.png"
@@ -23,19 +28,23 @@ function Home() {
     return (
         <>
             <main className="h-dvh flex flex-col gap-8 ">
+
                 <div className="h-30 self-end mr-8 " style={{ top: "0" }}>
                     <NavBar>
                         <a href="#home"><p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Inicio</p></a>
                         <a href="#projects"><p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Projectos</p></a>
                         <a href="#technologies"><p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Tecnologias</p></a>
-                        <p className='hover:cursor-pointer hover:text-blue-400 duration-305'>Contacto</p>
                         <select className="hover:text-blue-400 cursor-pointer" name="" id="">
                             <option className='text-black' value="">Español</option>
                             <option className='text-black' value="">English</option>
                         </select>
 
                     </NavBar>
+
                 </div>
+                <br />
+                <br />
+                <br />
 
                 <section id="home">
                     <div className=" lg:w-300 flex justify-center">
@@ -63,25 +72,94 @@ function Home() {
                     </div>
                 </section>
 
-                <div className=" min-h-20 bg-amber-50 flex justify-center items-center text-4xl ">
-                    <h1 className="drop-shadow-lg font-bold text-shadow-8xs" style={{
-                        color: "#279EFF", WebkitTextStroke: "1px black"
-                    }}>Proyectos</h1>
-                </div>
 
 
-                <section id="projects" className="flex flex-col lg:flex-row gap-10 justify-center items-center">
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
+                <section id="proyectos" className="flex items-center gap-4 my-12 px-4">
+                    <div className="flex-grow border-t border-gray-600"></div>
+                    <h2 className="text-3xl font-bold text-blue-400 whitespace-nowrap">Proyectos</h2>
+                    <div className="flex-grow border-t border-gray-600"></div>
                 </section>
 
-                <div className=" min-h-20 bg-amber-50 flex justify-center items-center text-4xl ">
-                    <h1 className="drop-shadow-lg font-bold text-shadow-8xs" style={{
-                        color: "#279EFF", WebkitTextStroke: "1px black"
-                    }}>Tecnologías</h1>
-                </div>
+
+                <section id="projects" className="w-250 flex-wrap place-self-center  gap-10 justify-center items-center">
+
+                    <div>
+                        <Card
+                            img={inmovw}
+                            projectName="Inmovw"
+                            description="Aplicación web para la gestión de propiedades inmobiliarias. Desarrollada con React, CSS y Supabase. Permite a los usuarios buscar y filtrar propiedades mientras ."
+                        >
+                            <div className="flex gap-4 w-fit">
+
+                                <div style={{ borderRadius: "8px" }} className="w-30 flex place-content-center bg-blue-700 text-amber-50 ">
+                                    <img className="w-8" src={reactjs} alt="" />
+                                    <p className="mt-0.5">React js</p>
+                                </div>
+
+                                <div style={{ borderRadius: "8px" }} className="w-30 flex place-content-center bg-blue-950 text-amber-50 ">
+                                    <img className="w-6 h-6 place-self-center" src={ts} alt="" />
+                                    <p className="mt-0.5">Typescript</p>
+                                </div>
+
+                                <div style={{ borderRadius: "8px" }} className="w-30 flex place-content-center bg-green-700 text-amber-50 ">
+                                    <img className="w-8" src={supabase} alt="" />
+                                    <p className="mt-0.5">Supabase</p>
+                                </div>
+
+                                <div style={{ borderRadius: "8px" }} className="w-30 flex place-content-center bg-blue-700 text-amber-50 ">
+                                    <img className="w-8" src={css} alt="" />
+                                    <p className="mt-0.5">Css</p>
+                                </div>
+
+                            </div>
+
+                        </Card>
+                    </div>
+
+                    <br />
+                    <br />
+                    <br />
+
+                    <div>
+                        <Card
+                            img={blackjack}
+                            projectName="Blackjack"
+                            description="Juego de cartas Blackjack, desarrollado con React y Tailwind CSS. El objetivo es sumar 21 puntos o lo más cercano posible sin pasarse."
+                        >
+                            <div style={{ borderRadius: "8px" }} className="w-30 flex place-content-center bg-blue-700 text-amber-50 ">
+                                <img className="w-8" src={reactjs} alt="" />
+                                <p className="mt-0.5">React js</p>
+                            </div>
+
+                            <div style={{ borderRadius: "8px" }} className="w-30 flex place-content-center bg-blue-950 text-amber-50 ">
+                                <img className="w-8" src={tailwind} alt="" />
+                                <p className="mt-0.5">Tailwind</p>
+                            </div>
+
+                            <div style={{ borderRadius: "8px" }} className="w-30 flex place-content-center bg-blue-700 text-amber-50 ">
+                                <img className="w-8" src={css} alt="" />
+                                <p className="mt-0.5">Css</p>
+                            </div>
+
+                        </Card>
+                    </div>
+
+
+                    <br />
+                    <br />
+                    <br />
+
+                    <div>
+                        {/* <Card></Card> */}
+                    </div>
+
+                </section>
+
+                <section id="proyectos" className="flex items-center gap-4 my-12 px-4">
+                    <div className="flex-grow border-t border-gray-600"></div>
+                    <h2 className="text-3xl font-bold text-blue-400 whitespace-nowrap">Tecnologias</h2>
+                    <div className="flex-grow border-t border-gray-600"></div>
+                </section>
 
                 <section id="technologies">
 
@@ -131,24 +209,30 @@ function Home() {
                     </div>
                 </section>
 
-                <div className=" min-h-0.5 bg-amber-50 flex justify-center items-center text-4xl "></div> {/** <-- separador */}
+                {/**separador */}
+                <section id="proyectos" className="flex items-center gap-0 my-12 px-4">
+                    <div className="flex-grow border-t border-gray-600"></div>
+                    <div className="flex-grow border-t border-gray-600"></div>
+                </section>
+                {/**separador */}
 
-                <section className="flex gap-33 text-amber-50 lg:flex-row gap-10 justify-center items-center">
-                    <div className="w-25 text-center group">
+                <section id="contact" className=" flex gap-33 text-amber-50 lg:flex-row justify-center items-center">
+                    <div className="w-10 text-1xl text-center group">
                         <img src={github} alt="" className="brightness-0 invert hover:brightness-100 hover:invert-0 transition-all duration-300" />
                         <p>GitHub</p>
                     </div>
 
-                    <div className="w-25 text-center group">
+                    <div className="w-10 text-1xl text-center group">
                         <img src={linkedin} alt="" className="brightness-0 invert hover:brightness-100 hover:invert-0 transition-all duration-300" />
                         <p>LinkedIn</p>
                     </div>
 
-                    <div className="w-25 text-center group">
-                        <img src={email} alt="" className="brightness-0 invert hover:brightness-100 hover:invert-0 transition-all duration-300" />
+                    <div className="w-10 text-1xl text-center group">
+                        <img src={email} alt="" className="brightness-0 invert hover:brightness-100 hover:invert-0 duration-300" />
                         <p>Email</p>
                     </div>
                 </section>
+
 
             </main >
         </>
--- src/assets/components/Card.tsx
+++ src/assets/components/Card.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import ejemplo from "../images/image 1.png"
 import image from "../images/github.svg"
-function Card() {
+function Card({ img, projectName, description, children }: { img: string, projectName: string, description: string, children: React.ReactNode }) {
     return (
         <>
-            <main className='w-90 h-150 rounded-2xl hover:-translate-y-5 duration-100 ' style={{ backgroundColor: "#1B293B" }}>
-                <img className='rounded-3xl w-90 flex place-self-center p-2' src={ejemplo} alt="" />
-                <h2 className='text-4xl font-bold place-self-center' style={{ color: "#279EFF", WebkitTextStroke: "1px black" }} >BlackJack</h2>
-                <p className='text-amber-50 text-2xl text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eum ullam, laudantium amet qui facilis asperiores at aperiam quo maiores ipsum repellat, corrupti aliquid eligendi cupiditate, mollitia in voluptate voluptatem?</p>
-                <button className='bg-amber-50 lg:w-30 h-10 hover:cursor-pointer text-2xl flex mt-5 place-self-center' style={{ borderRadius: "6px" }}>
-                    <img src={image} alt="" />
-                    github</button>
-            </main>
+            <section className='lg:flex gap-4'>
+                <div>
+                    <img className='rounded-3xl max-w-130 min-w-130 flex place-self-center p-2' src={img} alt="" />
+                </div>
+
+                <div>
+                    <div className='flex flex-col gap-4 lg:place-content-center'>
+                        <h2 className='h-fit text-4xl font-bold' style={{ color: "#279EFF", WebkitTextStroke: "1px black" }} >{projectName}</h2>
+                        <div className='lg:flex lg:gap-5'>
+                            {children}
+                        </div>
+                    </div>
+                    <br /><br />
+                    <p className='text-amber-50 text-2x1 '>{description}</p>
+                </div>
+            </section>
         </>
     )
 }
